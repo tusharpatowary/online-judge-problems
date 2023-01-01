bdcd8d8f43a0cc687eba1a80342e3edda1c38579
tusharpatowary <tushar114220@gmail.com>
2023-01-01 18:27:54 +0600

    beecrowd 1001 added in the repo

---
+++ beecrowd/c/1001-Extremely-Basic.c
@@ -0,0 +1,36 @@
+/*
+
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+Obs.: don't forget the endline after all.
+
+Samples Input |	Samples Output
+10            |
+9             |   X = 19
+              |
+-10           |
+4             |   X = -6
+______________|_____________________
+15            |
+-7            |   X = 8
+______________|_____________________
+
+
+*/
+
+// Solve------------------
+#include<stdio.h>
+int main()
+{
+    int A,B,X;
+    scanf("%d%d",&A,&B);
+    X=A+B;
+    printf("X = %d\n",X);
+    return 0;
+}
