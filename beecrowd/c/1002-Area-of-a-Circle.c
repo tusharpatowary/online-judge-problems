4ee679e8731e7241f01f7ad1af30fc6af2c6ca0a
tusharpatowary <tushar114220@gmail.com>
2023-01-02 20:20:34 +0600

    solution code added for 1002 area of a circle

--- beecrowd/c/1002-Area-of-a-Circle.c
+++ beecrowd/c/1002-Area-of-a-Circle.c
@@ -17,3 +17,13 @@ Input Samples	Output Samples
 
 
 */
+// SolutionCode-------------------
+#include<stdio.h>
+main()
+{
+    double R,A,p=3.14159;
+    scanf("%lf",&R);
+    A=p*R*R;
+    printf("A=%.4lf\n",A);
+    return 0;
+}
