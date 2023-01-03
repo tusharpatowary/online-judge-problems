05097b77ce27571d8b77587d99688d95a71fd120
tusharpatowary <tushar114220@gmail.com>
2023-01-03 20:25:34 +0600

    solution code added 1003 simple sum

--- beecrowd/c/1003 Simple Sum.c
+++ beecrowd/c/1003 Simple Sum.c
@@ -26,3 +26,14 @@ SOMA = -20
 SOMA = 0
 
 */
+
+// solutionCode
+#include<stdio.h>
+int main()
+{
+    int A,B,C;
+    scanf("%d%d",&A,&B);
+    C=A+B;
+    printf("SOMA = %d\n",C);
+    return 0;
+}
