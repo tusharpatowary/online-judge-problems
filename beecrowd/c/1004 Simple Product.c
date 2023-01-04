756b60079ee1feda11f1a2cc0e6f81bb91d0ce7b
tusharpatowary <tushar114220@gmail.com>
2023-01-04 20:30:15 +0600

    solution of simple product added in the repo

--- beecrowd/c/1004 Simple Product.c
+++ beecrowd/c/1004 Simple Product.c
@@ -26,3 +26,13 @@ PROD = -300
 PROD = 0
 
 */
+// solutionCode
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("PROD = %d\n",c);
+    return 0;
+}
