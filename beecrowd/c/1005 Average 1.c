34f60f25c8216c7a72dfbc079734ae8defa07c4a
tusharpatowary <tushar114220@gmail.com>
2023-01-05 20:42:10 +0600

    solution of average 1 added in the repo

--- beecrowd/c/1005 Average 1.c
+++ beecrowd/c/1005 Average 1.c
@@ -26,3 +26,13 @@ MEDIA = 4.84091
 MEDIA = 10.00000
 
 */
+// solutionCode
+#include<stdio.h>
+main()
+{
+    double A,B,C;
+    scanf("%lf%lf",&A,&B);
+    C=((A*3.5)+(B*7.5))/11;
+    printf("MEDIA = %.5lf\n",C);
+    return 0;
+}
