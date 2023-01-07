e3a4cbdfc855a890630437fd152bdbe412ebb4c5
tusharpatowary <tushar114220@gmail.com>
2023-01-07 20:46:41 +0600

    solution of average 2 added in the repo

--- beecrowd/c/1006 Average 2.c
+++ beecrowd/c/1006 Average 2.c
@@ -30,3 +30,13 @@ MEDIA = 7.5
 
 
 */
+// solutionCode
+#include<stdio.h>
+main()
+{
+    float A,B,C,D;
+    scanf("%f%f%f",&A,&B,&C);
+    D=((A*2)+(B*3)+(C*5))/10;
+    printf("MEDIA = %.1f\n",D);
+    return 0;
+}
