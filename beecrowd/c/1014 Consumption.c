f25709fa2ce64f38b028cf3e7cf971604791adf9
tusharpatowary <tushar114220@gmail.com>
2023-01-16 21:37:57 +0600

    1014 added in the repo

--- beecrowd/c/1014 Consumption.c
+++ beecrowd/c/1014 Consumption.c
@@ -26,3 +26,14 @@ Output Sample
 9.802 km/l
 
 */
+// solutionCode
+#include<stdio.h>
+main()
+{
+    int X;
+    float Y,o;
+    scanf("%d%f",&X,&Y);
+    o=X/Y;
+    printf("%.3lf km/l\n",o);
+    return 0;
+}
